83155b89307a4e4fc6473033953fa829d99db912
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-15 06:21:54 -0700

    d

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-/* Main funct: no hace nada. */
+/**
+ *  Main funct: no hace nada. 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
