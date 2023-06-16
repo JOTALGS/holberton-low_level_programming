77a42d2ef3a111b649a816bb3a6e81a7db295dee
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 12:26:45 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - abs val
+ * @int: int
+ * Return:abs of an int
+ */
+int _abs(int n)
+{
+	int a;
+
+	a = abs(n);
+	return (a);
+}
