575477a506852cfa3de1acf34127db55c17f26a4
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 10:24:31 -0700

    das

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - check the nest
+ */
+void print_alphabet(void)
+{
+	char alph;
+	int ct;
+
+	for (ct = 0; ct <= 9; ct++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}
+
