d0b2f4ac22778e03d553c9c0a9b10798197118d4
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 07:51:48 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function stuff
+ * Return:0
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = '0'; dgt <= '9'; dgt++)
+	{
+		putchar(dgt);
+	}
+	putchar('\n');
+	return (0);
+}
+
