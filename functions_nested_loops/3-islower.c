776595376a446959456dc834038c915ab0063b88
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 11:14:04 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _islower - dale qnas
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
