865338aa266e542fcf542d7ad3ba52f7a57e8f13
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 07:38:14 -0700

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function stuff
+ * Return:0
+ */
+int main(void)
+{
+	char abc;
+	char newline = '\n';
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if ((abc == 'e') | (abc == 'q'))
+		{
+		} else
+		{
+			putchar(abc);
+		}
+	}
+	putchar(newline);
+	return (0);
+}
+
