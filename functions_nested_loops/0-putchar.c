feb8c5da712ac0cc6a1c2d29ee757edccf563e02
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-16 09:36:15 -0700

    try

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - nana main stuff
+ * Return:0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * nest - nested under main
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char alph = "abcdefghijklmnopqrstuvwxyz";
+
+	_putchar(aplh);
+	return (0);
+}
+
