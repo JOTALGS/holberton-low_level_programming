a9455fa95b6953db5b885335fd9810780ab1c73d
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-18 14:31:19 -0700

    8fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * jack_bauer - sabemo qie ne
  */
 void jack_bauer(void)
 {
-	int hr;
-	int hr_1;
-	int mn;
-	int mn_1;
+	char hr;
+	char hr_1;
+	char mn;
+	char mn_1;
 
-	for (hr = 0; hr <= 9; hr++)
+	for (hr = '0'; hr <= '9'; hr++)
 	{
-		for (hr_1 = 0; hr_1 <= 9; hr_1++)
+		for (hr_1 = '0'; hr_1 <= '9'; hr_1++)
 		{
-			for (mn = 0; mn <=9; mn++)
+			for (mn = '0'; mn <= '9'; mn++)
 			{
-				for (mn_1 = 0; mn_1 <= 9; mn_1++)
+				for (mn_1 = '0'; mn_1 <= '9'; mn_1++)
 				{
 					_putchar(hr);
 					_putchar(hr_1);
