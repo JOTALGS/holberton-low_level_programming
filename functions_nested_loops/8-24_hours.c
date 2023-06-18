560484f9990701b07c14222036a5f7a0b1c7414b
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-18 14:48:35 -0700

    8fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,20 +10,23 @@ void jack_bauer(void)
 	char mn;
 	char mn_1;
 
-	for (hr = '0'; hr <= '9'; hr++)
+	for (hr = '0'; hr <= '2'; hr++)
 	{
 		for (hr_1 = '0'; hr_1 <= '9'; hr_1++)
 		{
-			for (mn = '0'; mn <= '9'; mn++)
+			for (mn = '0'; mn <= '5'; mn++)
 			{
 				for (mn_1 = '0'; mn_1 <= '9'; mn_1++)
 				{
-					_putchar(hr);
-					_putchar(hr_1);
-					_putchar(':');
-					_putchar(mn);
-					_putchar(mn_1);
-					_putchar('\n');
+					if (hr_1 < '4' && hr <= '2')
+					{
+						_putchar(hr);
+						_putchar(hr_1);
+						_putchar(':');
+						_putchar(mn);
+						_putchar(mn_1);
+						_putchar('\n');
+					}
 				}
 			}
 		}
