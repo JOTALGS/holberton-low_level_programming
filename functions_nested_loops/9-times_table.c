37c758cb72b986358be00e968e63a284f1b71fd2
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-18 16:48:14 -0700

    9fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,17 +9,17 @@ void times_table(void)
 	int j;
 	int n = 9;
 
-	for (i = 0; i <= n; i++) 
+	for (i = 0; i <= n; i++)
 	{
 		printf("%d", 0);
 		printf(",");
-		for (j = 1; j <= n; j++) 
+		for (j = 1; j <= n; j++)
 		{
 			printf("%3d", i * j);
 			if (j != n)
 			{
 				printf(",");
-			}	
+			}
 		}
 		printf("\n");
 	}
