34ee1739a57e52e5499d1f2ea2163b97f71e7f2d
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-20 07:25:50 -0700

    vbvV

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - sdzazda
+ */
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+	}
+}
