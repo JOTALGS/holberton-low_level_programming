a3b8763111e972b3b37f822e2e37ee1517dfde27
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-20 08:04:35 -0700

    w

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - sdasdsadd
+ * @n: skdksad
+ */
+void print_diagonal(int n)
+{
+	int c;
+	int y;
+
+	for (c = 0; c <=n; c++)
+	{
+		for (y = 0; y <= c ; y++)
+		{
+			_putchar (' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
