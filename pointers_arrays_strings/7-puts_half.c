cba1df742f04f66aa54098118d5e14ca7cb6aa89
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-26 10:13:37 -0700

    f

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -18,9 +18,11 @@ puts_half(char *s)
 	}
 	else
 	{
-		for (i = ((length - 1 / 2)); i <= length; i++)
+		for (i = ((length - 1) / 2) + 1; i <= length; i++)
 		{
 			putchar(s[i]);
+		
 		}
+		putchar('\n');
 	}
 }
