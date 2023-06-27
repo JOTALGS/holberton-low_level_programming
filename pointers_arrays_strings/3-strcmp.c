46bb1186569902b7cc006e51bcae2bd0f12fbc6f
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-27 08:33:51 -0700

    s

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@ _strcmp(char *s1, char *s2)
 	char b;
 	int i = 0;
 
-	do{
+	do {
 		a = s1[i];
 		b = s2[i];
 		if (a == '\0')
@@ -16,8 +16,8 @@ _strcmp(char *s1, char *s2)
 			return (a - b);
 		}
 		i++;
-	}
-	while (a == b);
+	} while (a == b);
+	
 	{
 	}
 	return (a - b);
