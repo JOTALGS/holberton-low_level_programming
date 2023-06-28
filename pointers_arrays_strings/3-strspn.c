9bae33be7a961764e0a8fa2d74ac48151af3ab9f
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-28 06:56:02 -0700

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,9 +9,9 @@ _strspn(char *s, char *accept)
 	int conf = 0;
 
 	ct = 0;
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (y = 0; s[y] != '\0'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (s[y] == accept[i])
 			{
