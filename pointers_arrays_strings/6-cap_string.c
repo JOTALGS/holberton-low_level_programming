a8a1e26c1b8da7b1bf51391ed27c63a6ca48bba2
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-27 17:46:27 -0700

    23!

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+char
+*cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\n')
+		{
+			if (isalpha(str[i + 1]) == 0)
+			{
+				str[i + 1] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
