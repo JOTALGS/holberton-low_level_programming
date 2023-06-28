f3b76737e664051249f819dc19d6c8809af42bbe
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-27 18:12:41 -0700

    s

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,12 +13,16 @@ char
 		{
 			if (i == 0)
 			{
-				str[i] = str[i] -32;
+				str[i] = str[i] - 32;
 			}
-			else if (str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '\t' || str[i - 1] == '.')
+			else if (str[i - 1] == ' ' || str[i - 1] == '\n')
 			{
 				str[i] = str[i] - 32;
 			}
+			else if (str[i - 1] == '\t' || str[i - 1] == '.')
+                        {
+                                str[i] = str[i] - 32;
+                        }
 		}
 	}
 	return (str);
