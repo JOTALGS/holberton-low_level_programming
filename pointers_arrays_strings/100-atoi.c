444b5c2e4d73a4bebc5522f4b17e3fb5140e0d3a
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-27 19:26:23 -0700

    f

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ _atoi(char *s)
 	int len = strlen(s);
 	int len_1;
 	int i;
-	int c;
+	int sgn = 1;
 	char str[500] = "s";
 	int ind = 0;
 
@@ -18,19 +18,29 @@ _atoi(char *s)
 	{
 		if (isdigit(s[i]))
 		{
-			c = i - 1;
-			if (s[c] == '+' || s[c] == '-')
+			if (s[i - 1] == ' ')
 			{
-				str[ind] = s[c];
+				sgn = 1;
+			}
+			else if (s[i -1] == '-')
+			{
+				sgn = - 1;
 			}
 			str[ind] = s[i];
+			ind++;
+			if (isdigit(s[i + 1]))
+			{
+			}
+			else
+			{
+				break;
+			}
 		}
 	}
 	len_1 = strlen(str);
-	puts(str);
 	if (len_1 != 0)
 	{
-		return (atoi(str));
+		return (atoi(str) * sgn);
 	}
 	else
 	{
