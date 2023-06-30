c6809dd8d804842ffb7f2943312982bf16d4589f
JOTALGS <jose,pgilsuarez@gmail.com>
2023-06-30 06:36:15 -0700

    $2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -5,12 +5,9 @@ main(int argc, char **argv)
 {
 	int i;
 
-	if (argc > 0)
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
