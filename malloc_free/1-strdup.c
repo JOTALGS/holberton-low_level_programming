7a4bf6b60019121bb7df97aadbe07625394ef77e
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-04 18:34:56 -0700

    $2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	new_str = malloc(sizeof(str) + 1);
+	new_str = malloc(sizeof(str));
 	if (!new_str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
