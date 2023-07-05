5d9b5d48620858b8f47609620fb6a7b6e34b0c38
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-04 18:31:49 -0700

    $2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - jsjdj
+ * @str: stre
+ * Return: bla.
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	new_str = malloc(sizeof(str) + 1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		new_str[i] = str[i];
+	}
+	new_str[i + 1] = '\0';
+	return (new_str);
+}
