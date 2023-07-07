55f72c78750f8962ffa638c1ba4c93be04cb80f9
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-07 07:44:45 -0700

    $2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,30 +12,33 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int len1, len2, lenc;
-        char *new_str;
-        int i = 0;
-        int c = 0;
+	unsigned int len1, len2, lenc;
+	char *new_str;
+	unsigned int i = 0;
+	unsigned int c = 0;
 
-        if (!s1)
-                s1 = "";
-        if (!s2)
-                s2 = "";
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
-        len1 = strlen(s1);
-        len2 = strlen(s2);
-        if (n > len2)
-                n = len2;
-        lenc = len1 + n;
-        new_str = malloc(sizeof(char) * (lenc + 1));
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n > len2)
+		n = len2;
+	lenc = len1 + n;
+	new_str = malloc(sizeof(char) * (lenc + 1));
+	if (!new_str)
+		return (NULL);
 
-        for (i = 0; i < len1; i++)
-                new_str[i] = s1[i];
-        for (c = 0; c < n; c++)
-        {
-                i++;
-                new_str[i] = s2[c];
-        }
-        new_str[i] = '\0';
-        return (new_str);
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
+
+	for (c = 0; c < n; c++)
+	{
+		new_str[i] = s2[c];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
