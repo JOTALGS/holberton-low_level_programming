3c59a148e41e3fa44a42461fbf6dc194fd0ba477
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-09 13:55:06 -0700

    $2

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -7,15 +7,20 @@ array_range(int min, int max)
 	int *arr;
 	int i;
 	int st = min;
+	int ct = 0;
 
-	if(min > max)
+	if (min > max)
 		return (NULL);
-	while(st < max)
+
+	while (st < max)
+	{
+		ct++;
 		st++;
-	arr = malloc((st + 1) * sizeof(int));
-	if(!arr)
+	}
+	arr = malloc((ct + 1) * sizeof(int));
+	if (!arr)
 		return (NULL);
-	for(i = 0; i <= st; i++)
+	for (i = 0; i <= ct; i++)
 		arr[i] = min + i;
 	return (arr);
 }
