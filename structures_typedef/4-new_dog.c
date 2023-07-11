29dadcc02e1edbc2658a5c0fb1ab90031de7ce81
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-11 10:15:30 -0700

    $2

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+dog_t *
+new_dog(char *name, float age, char *owner)
+{
+	char *n_cpy;
+	char *o_cpy;
+	
+	dog_t *new_d;
+
+	n_cpy = malloc(strlen(name) + 1);
+	if (!n_cpy)
+		return (NULL);
+	o_cpy = malloc(strlen(owner) + 1);
+	if (!o_cpy)
+		return (NULL);
+
+	strcpy(n_cpy, name);
+	strcpy(o_cpy, owner);
+
+	new_d = malloc(sizeof(struct dog));
+	if (!new_d)
+		return (NULL);
+
+	(*new_d).name = n_cpy;
+	(*new_d).age = age;
+	(*new_d).owner = o_cpy;
+
+	return (new_d);
+}
