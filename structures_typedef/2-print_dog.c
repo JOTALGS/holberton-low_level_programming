727b278842c44829a90502144c1882ded26edd1b
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-11 08:47:36 -0700

    $2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,14 +9,16 @@ print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
+
 		if (d->age)
 			printf("Age: %f\n", d->age);
-                else
-                        printf("Name: (nil)");
+		else
+			printf("Age: (nil)\n");
+
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
-                else
-                        printf("Name: (nil)");
+		else
+			printf("Owner: (nil)\n");
 	}
 }
