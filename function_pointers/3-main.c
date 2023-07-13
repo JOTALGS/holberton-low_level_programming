826211c02f6bae50de2e3914561a29efdeb7bd59
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-13 08:49:26 -0700

    $2

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+	if (!(*argv[2] == '+' && *argv[2] == '-' && *argv[2] == '*' && *argv[2] == '/' && *argv[2] == '%'))
 	{
-                printf("Error: \n");
+                printf("Error\n");
                 exit(99);
         }
 	if ((*argv[2] == '/' && *argv[3] == '0') || (*argv[2] == '%' &&  *argv[3] == '0'))
