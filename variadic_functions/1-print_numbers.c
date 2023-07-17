b24c6f7339e219f96581fd326b435ff51841af9a
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-17 09:40:18 -0700

    $2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,20 +8,24 @@ print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator)
+	if (n != 0)
 	{
-		for (i = 0; i < n; i++)
+		if (separator)
 		{
-			printf("%d", va_arg(args, int));
-			if (i != n - 1)
-				printf("%s", separator);
-			else
-				printf("\n");
+			for (i = 0; i < n; i++)
+			{
+				printf("%d", va_arg(args, int));
+				if (i != n - 1)
+					printf("%s", separator);
+				else
+					printf("\n");
+			}
 		}
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-			printf("%d", va_arg(args, int));
+		else
+		{
+			for (i = 0; i < n; i++)
+				printf("%d", va_arg(args, int));
+			printf("\n");
+		}
 	}
 }
