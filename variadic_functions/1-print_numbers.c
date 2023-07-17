cd3f8de7de3b270545c6119234dbc951727df332
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-17 09:36:19 -0700

    $2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void
+print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (separator)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (i != n - 1)
+				printf("%s", separator);
+			else
+				printf("\n");
+		}
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			printf("%d", va_arg(args, int));
+	}
+}
