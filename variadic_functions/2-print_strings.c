d13cb3cde4475c2542853555a67014ff433cd460
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-17 18:43:00 -0700

    $2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ print_strings(const char *separator, const unsigned int n, ...)
 			for (i = 0; i < n; i++)
 			{
 				str = va_arg(args, char *);
-				if(str)
+				if (str)
 				{
 					printf("%s", str);
 					if (i != n - 1)
