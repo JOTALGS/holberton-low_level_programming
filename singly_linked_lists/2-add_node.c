bfa02c9c7a4a13e31eba91a81b09d468a849069b
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-26 09:49:38 -0700

    2 betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-list_t *add_node(list_t **head, const char *str)
+list_t
+*add_node(list_t **head, const char *str)
 {
 	list_t *temp;
-	
+
 	temp = malloc(sizeof(list_t));
 	if (!temp)
 		return (NULL);
