cc000c7a592fad5ea75b76b75ebf1304aef66aea
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-26 09:48:20 -0700

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
