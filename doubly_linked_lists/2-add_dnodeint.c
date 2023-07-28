32d7614a7632af27526124082f489d601be1a970
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-28 13:33:58 -0700

    2fix@:

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,11 +5,16 @@ dlistint_t
 *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *temp;
+	dlistint_t *ptr = *head;
 
 	temp = malloc(sizeof(dlistint_t));
+	if (!temp)
+		return (0);
 	temp->n = n;
 	temp->next = *head;
 	temp->prev = NULL;
+	if (ptr)
+		ptr->prev = temp;
 	*head = temp;
 	return (temp);
 }
