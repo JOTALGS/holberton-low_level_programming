dfa1d4aa6bb981509f4f91489b38ff4c008a39a6
JOTALGS <jose,pgilsuarez@gmail.com>
2023-08-02 08:33:43 -0700

    1fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,9 +13,9 @@ create_file(const char *filename, char *text_content)
 	int fd1, w;
 	int len = strlen(text_content);
 
-	if (!filename | !text_content)
+	if (!filename)
 		return (-1);
-	fd1 = open(filename, O_CREAT|O_WRONLY, 600);
+	fd1 = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 600);
 	w = write(fd1, text_content, len);
 	if ((fd1 < 0) | (w < 0))
 		return (-1);
