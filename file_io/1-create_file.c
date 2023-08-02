abe774e27062f9097cb8e3cfffdfc9e80c16ecaa
JOTALGS <jose,pgilsuarez@gmail.com>
2023-08-02 08:39:18 -0700

    1fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd1 = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		write(fd1, "", 0);
 		close(fd1);
