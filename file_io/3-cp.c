51ddb892a75c69193e18ab08dedd3ab309a037bb
JOTALGS <jose,pgilsuarez@gmail.com>
2023-08-03 10:14:19 -0700

    3 fix 2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,24 +4,34 @@
 void
 copy_file(const char *from, const char *to)
 {
-	int fd, fd1, w, c;
+	int fd, fd1, w, c, r;
 	char *buf[1024];
 
 	fd = open(from, O_RDONLY);
+	
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
-	read(fd, buf, 1024);
+	r = read(fd, buf, 1024);
 	fd1 = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0662);
-	w = write(fd1, buf, 1024);
+	
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+		exit(98);
+	}
+	else if (r > 0)
+		w = write(fd1, buf, 1024);
+	
 	if (w == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
 	c = close(fd1);
+	
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
