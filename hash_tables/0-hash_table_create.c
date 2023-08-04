8b77d0f81a8a4b34b219150132e9c30e6af384b5
JOTALGS <jose,pgilsuarez@gmail.com>
2023-08-04 06:20:22 -0700

    0:

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,10 +5,15 @@ hash_table_t
 *hash_table_create(unsigned long int size)
 {
 	hash_table_t *nd;
+	hash_node_t **arr;
 
 	nd = malloc(sizeof(hash_table_t));
 	if (!nd)
 		return (NULL);
 	nd->size = size;
+	arr = malloc(sizeof(char) * size);
+	if (!arr)
+		return (NULL);
+	nd->array = arr;
 	return (nd);
 }
