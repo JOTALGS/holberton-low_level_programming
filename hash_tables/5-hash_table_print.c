672490f9f16306fe0d9f75c77993bb1c0137ab91
JOTALGS <jose,pgilsuarez@gmail.com>
2023-08-07 16:59:22 -0700

    5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+void
+hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	char n;
+
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		printf("[%lu]", i);
+		n = 0;
+		while (p)
+		{
+			if (n)
+				printf(", ");
+			printf("'%s': '%s'", p->key, p->value);
+			p = p-> next;
+			n = 1;
+		}
+		printf("\n");
+	}
+}
