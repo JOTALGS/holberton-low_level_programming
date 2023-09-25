0063cc4eca9dec30949fe797d275fda6fc01e3f1
JOTALGS <jose.pgilsuarez@gmail.com>
2023-09-24 21:06:23 -0300

    hh

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,41 +1,27 @@
 #include "search_algos.h"
 
-int binary_search(int *array, size_t size, int value) {
+int
+binary_search(int *array, size_t size, int value)
+{
+	size_t first = 0;
+	size_t last = 0;
+	size_t mid = 0;
 
-int i;
-int left = 0;
-int right = size - 1;
-int mid;
+	if (array == NULL || size == 0)
+		return (-1);
 
-if (array == NULL || size == 0) {
-return -1;
-}
+	last = size - 1;
+	while (first <= last)
+	{
+		print_array(array, first, last);
+		mid = (first + last) / 2;
 
-
-
-while (left <= right) {
-mid = left + (right - left) / 2;
-
-printf("Searching: ");
-for (i = 0; i < size; i++) {
-if (i == mid) {
-printf("| ");
-}
-printf("%d ", array[i]);
-}
-printf("\n");
-
-if (array[mid] == value) {
-return mid;
-}
-
-if (array[mid] > value) {
-right = mid - 1;
-}
-else {
-left = mid + 1;
-}
-}
-
-return -1;
+		if (array[mid] > value)
+			last = mid - 1;
+		else if (array[mid] < value)
+			first = mid + 1;
+		else
+			return (mid);
+	}
+	return (-1);
 }
